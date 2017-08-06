15f686876d696d3241ab537b4594f44fa098dff1
ofkindness <andrei.tretyakov@gmail.com>
2017-08-06 19:45:13 +0300

    Feature/issue 1 test (#4)
    *  - Add winston test
    *  - Update dependencies
    *  - Update README, LICENCE, 
    *  - Update version to 2.2.3

--- test/winston-pg-native-test.js
+++ test/winston-pg-native-test.js
@@ -8,41 +8,18 @@
 
 var vows = require('vows');
 var transport = require('winston/test/transports/transport');
-var Postgres = require('../lib/winston-pg-native');
-// var Promise = require('bluebird');
-// var winston = require('winston');
-// var pg = require('pg').native;
+var Postgres = require('../lib/winston-pg-native.js');
 
-var conString = "postgres://" + process.env.POSTGRES_USER +
-  ":" + process.env.POSTGRES_PASSWORD +
-  "@" + process.env.POSTGRES_HOST +
-  ":" + process.env.POSTGRES_PORT +
-  "/" + process.env.POSTGRES_DBNAME;
+var conString = 'postgres://' + process.env.POSTGRES_USER +
+':' + process.env.POSTGRES_PASSWORD +
+'@' + process.env.POSTGRES_HOST +
+':' + process.env.POSTGRES_PORT +
+'/' + process.env.POSTGRES_DBNAME;
 
 vows.describe('winston-pg-native')
   .addBatch({
     'An instance of the Postgres Transport': transport(Postgres, {
-      conString: conString,
-      tableConfig: {
-        tableName: 'winston_logs',
-        tableFields: 'level, msg, meta'
-      },
-      level: 'info'
+      conString: conString
     })
   })
-  // .addBatch({
-  // 'An instance of the Postgres Transport with promise':
-  //     transport(Postgres, {
-  //       conString: new Promise(function(resolve, reject) {
-  //         pg.connect(conString,
-  //             function(err, db) {
-  //               if (err) {
-  //                 reject(err);
-  //               } else {
-  //                 resolve(db);
-  //               }
-  //             });
-  //       })
-  //     })
-  // })
   .export(module);
